21f5b005391f44fa7067bd3eafb061d4ab59267c
Fikirat Javadov <iamjavadov@gmail.com>
2022-08-22 20:48:03 +0400

    Express-intro

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync("./tours-data.json"));
+
+app.get("/tour", (req, res) => {
+  res.json({
+    success: true,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get("/tour/:id", (req, res) => {
+  const id = req.params.id;
+
+  const tour = tours.find((t) => t.id == id);
+
+  if (!tour) return res.send({ success: false, message: "Invalid ID" });
+
+  res.json({ success: true, data: { tour } });
+});
+
+app.post("/tour", (req, res) => {
+  const id = tours[tours.length - 1].id + 1;
+
+  const newTour = { ...req.body, id: id };
+  const newTours = [...tours, newTour];
+
+  fs.writeFileSync("./tours-data.json", JSON.stringify(newTours));
+
+  res.json({
+    success: true,
+    data: {
+      newTour,
+    },
+  });
+});
+
+app.get("/tour/id", () => {});
+app.get("/tour/id", () => {});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
