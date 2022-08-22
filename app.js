05bde5babc25576b11993cd8f41d8df6f841c2da
Fikirat Javadov <iamjavadov@gmail.com>
2022-08-22 20:55:52 +0400

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync("./tours-data.json"));
